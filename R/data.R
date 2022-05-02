33703692ad7c704560ff3f47af28f90b9b31f309
Dekayzc <zhangchen3@bgi.com>
2022-05-02 19:36:27 +0800

    Add files via Git. Version 1.3

---
+++ R/data.R
@@ -0,0 +1,307 @@
+#' @docType data
+#' @name cell_e_gexp_eyes
+#'
+#' @title Exons matrix of monkey limbal cells
+#' @description  Exons matrix of monkey limbal cells containing gene and cell name
+#' rows are genes and columns are cells. 
+#'  
+#' @format A matrix with 17408 rows and 1417 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_e_gexp_eyes"
+
+#' @docType data
+#' @name cell_i_gexp_eyes
+#'
+#' @title Introns matrix of monkey limbal cells
+#' @description  Introns matrix of monkey limbal cells containing gene and cell name
+#' rows are genes and columns are cells. 
+#'  
+#' @format A matrix with 17408 rows and 1417 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_i_gexp_eyes"
+
+#' @docType data
+#' @name cell_s_gexp_eyes
+#'
+#' @title Spliced counts matrix of monkey limbal cells
+#' @description  Spliced counts matrix of monkey limbal cells containing gene and cell name
+#' rows are genes and columns are cells. 
+#'  
+#' @format A matrix with 17408 rows and 1417 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_s_gexp_eyes"
+
+#' @docType data
+#' @name cell_u_gexp_eyes
+#'
+#' @title Unspliced counts matrix of monkey limbal cells
+#' @description  Unspliced counts matrix of monkey limbal cells containing gene and cell name
+#' rows are genes and columns are cells. 
+#'  
+#' @format A matrix with 17408 rows and 1417 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_u_gexp_eyes"
+
+#' @docType data
+#' @name cell_RNA_gexp_eyes
+#'
+#' @title Expression matrix of monkey limbal cells
+#' @description  Expression matrix of monkey limbal cells containing gene and cell name
+#' rows are genes and columns are cells. 
+#'  
+#' @format A matrix with 17408 rows and 1417 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_RNA_gexp_eyes"
+
+#' @docType data
+#' @name cell_e_gexp_sper_ont_gene
+#'
+#' @title Exons matrix of mouse spermary cells using HISOFA-seq without gene clustering
+#' @description  Exons matrix of mouse spermary cells containing gene and cell name
+#' rows are genes and columns are cells. 
+#'  
+#' @format A matrix with 19221 rows and 3000 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_e_gexp_sper_ont_gene"
+
+#' @docType data
+#' @name cell_i_gexp_sper_ont_gene
+#'
+#' @title Introns matrix of mouse spermary cells using HISOFA-seq without gene clustering
+#' @description  Introns matrix of mouse spermary cells containing gene and cell name
+#' rows are genes and columns are cells. 
+#'  
+#' @format A matrix with 19221 rows and 3000 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_i_gexp_sper_ont_gene"
+
+#' @docType data
+#' @name cell_s_gexp_sper_ont_gene
+#'
+#' @title Spliced counts matrix of mouse spermary cells using HISOFA-seq without gene clustering
+#' @description  Spliced counts matrix of mouse spermary cells containing gene and cell name
+#' rows are genes and columns are cells. 
+#'  
+#' @format A matrix with 19221 rows and 3000 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_s_gexp_sper_ont_gene"
+
+#' @docType data
+#' @name cell_u_gexp_sper_ont_gene
+#'
+#' @title Unspliced counts matrix of mouse spermary cells using HISOFA-seq without gene clustering
+#' @description  Unspliced counts matrix of mouse spermary cells containing gene and cell name
+#' rows are genes and columns are cells. 
+#'  
+#' @format A matrix with 19221 rows and 3000 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_u_gexp_sper_ont_gene"
+
+#' @docType data
+#' @name cell_RNA_gexp_sper_ont_gene
+#'
+#' @title Expression matrix of mouse spermary cells using HISOFA-seq without gene clustering
+#' @description  Expression matrix of mouse spermary cells containing gene and cell name
+#' rows are genes and columns are cells. 
+#'  
+#' @format A matrix with 19221 rows and 3000 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_RNA_gexp_sper_ont_gene"
+
+#' @docType data
+#' @name cell_e_gexp_sper_ont_trans
+#'
+#' @title Exons matrix of mouse spermary cells using HISOFA-seq with gene clustering
+#' @description  Exons matrix of mouse spermary cells containing gene and cell name
+#' rows are genes and columns are cells. 
+#'  
+#' @format A matrix with 17721 rows and 3000 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_e_gexp_sper_ont_trans"
+
+#' @docType data
+#' @name cell_i_gexp_sper_ont_trans
+#'
+#' @title Introns matrix of mouse spermary cells using HISOFA-seq with gene clustering
+#' @description  Introns matrix of mouse spermary cells containing gene and cell name
+#' rows are genes and columns are cells. 
+#'  
+#' @format A matrix with 17721 rows and 3000 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_i_gexp_sper_ont_trans"
+
+#' @docType data
+#' @name cell_s_gexp_sper_ont_trans
+#'
+#' @title Spliced counts matrix of mouse spermary cells using HISOFA-seq with gene clustering
+#' @description  Spliced counts matrix of mouse spermary cells containing gene and cell name
+#' rows are genes and columns are cells. 
+#'  
+#' @format A matrix with 17721 rows and 3000 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_s_gexp_sper_ont_trans"
+
+#' @docType data
+#' @name cell_u_gexp_sper_ont_trans
+#'
+#' @title Unspliced counts matrix of mouse spermary cells using HISOFA-seq with gene clustering
+#' @description  Unspliced counts matrix of mouse spermary cells containing gene and cell name
+#' rows are genes and columns are cells. 
+#'  
+#' @format A matrix with 17721 rows and 3000 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_u_gexp_sper_ont_trans"
+
+#' @docType data
+#' @name cell_RNA_gexp_sper_ont_trans
+#'
+#' @title Expression matrix of mouse spermary cells using HISOFA-seq with gene clustering
+#' @description  Expression matrix of mouse spermary cells containing gene and cell name
+#' rows are genes and columns are cells. 
+#'  
+#' @format A matrix with 17721 rows and 3000 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_RNA_gexp_sper_ont_trans"
+
+#' @docType data
+#' @name cell_e_gexp_sper_pb
+#'
+#' @title Exons matrix of mouse spermary cells using HIT-scISOseq
+#' @description  Exons matrix of mouse spermary cells containing gene and cell name
+#' rows are genes and columns are cells. 
+#'  
+#' @format A matrix with 18255 rows and 3000 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_e_gexp_sper_pb"
+
+#' @docType data
+#' @name cell_i_gexp_sper_pb
+#'
+#' @title Introns matrix of mouse spermary cells using HIT-scISOseq
+#' @description  Introns matrix of mouse spermary cells containing gene and cell name
+#' rows are genes and columns are cells. 
+#'  
+#' @format A matrix with 18255 rows and 3000 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_i_gexp_sper_pb"
+
+#' @docType data
+#' @name cell_s_gexp_sper_pb
+#'
+#' @title Spliced counts matrix of mouse spermary cells using HIT-scISOseq
+#' @description  Spliced counts matrix of mouse spermary cells containing gene and cell name
+#' rows are genes and columns are cells. 
+#'  
+#' @format A matrix with 18255 rows and 3000 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_s_gexp_sper_pb"
+
+#' @docType data
+#' @name cell_u_gexp_sper_pb
+#'
+#' @title Unspliced counts matrix of mouse spermary cells using HIT-scISOseq
+#' @description  Unspliced counts matrix of mouse spermary cells containing gene and cell name
+#' rows are genes and columns are cells. 
+#'  
+#' @format A matrix with 18255 rows and 3000 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_u_gexp_sper_pb"
+
+#' @docType data
+#' @name cell_RNA_gexp_sper_pb
+#'
+#' @title Expression matrix of mouse spermary cells using HIT-scISOseq
+#' @description  Expression matrix of mouse spermary cells containing gene and cell name
+#' rows are genes and columns are cells. 
+#'  
+#' @format A matrix with 18255 rows and 3000 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_RNA_gexp_sper_pb"
+
+#' @docType data
+#' @name cell_ls_gexp_sper_pb
+#'
+#' @title Length of spliced counts matrix of mouse spermary cells using HIT-scISOseq
+#' @description  Length matrix of mouse spermary cells containing gene and cell name
+#' rows are genes and columns are cells, length is total length. 
+#'  
+#' @format A matrix with 18255 rows and 3000 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_ls_gexp_sper_pb"
+
+#' @docType data
+#' @name cell_lu_gexp_sper_pb
+#'
+#' @title Length of unspliced counts matrix of mouse spermary cells using HIT-scISOseq
+#' @description  Length matrix of mouse spermary cells containing gene and cell name
+#' rows are genes and columns are cells, length is total length. 
+#'  
+#' @format A matrix with 18255 rows and 3000 columns
+#' \describe{
+#'   ...
+#' }
+#' @source \url{https://github.com/Dekayzc/Regionvelocity}
+"cell_lu_gexp_sper_pb"
